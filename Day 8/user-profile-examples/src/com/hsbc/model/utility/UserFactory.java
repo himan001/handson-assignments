8135a0ded1a619005bcde541a42ec60809b6f9b4
himan001 <himanagrawal001@gmail.com>
2020-09-24 10:48:34 +0530

    File handling user profile

---
+++ Day 8/user-profile-examples/src/com/hsbc/model/utility/UserFactory.java
@@ -0,0 +1,26 @@
+package com.hsbc.model.utility;
+
+import com.hsbc.model.business.UserServiceImpl;
+import com.hsbc.model.dao.ArrayBackedUserDao;
+import com.hsbc.model.dao.CollectionBackedUserDao;
+import com.hsbc.model.dao.FileBackedUserDao;
+
+
+public class UserFactory {
+	// it can return dao, service or controller instance
+	
+	
+	public static Object getInstance(Type type) {
+		Object obj = null;
+		switch(type) {
+		case DAO : 
+			//obj = new CollectionBackedUserDao();
+			obj = new FileBackedUserDao();
+			break;
+		case SERVICE :
+			obj = new UserServiceImpl();
+			break;
+		}
+		return obj;
+	}
+}
